8a146e2d0a1f94113352d612950390a4ab7f0109
Sean A. Abdoli <mauveisfine@gmail.com>
2017-04-21 18:06:07 -0500

    Added values to seed file
    Added seed users, posts, and comments

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(
+  email: 'user@example.com',
+  password: 'password'
+)
+
+User.create(
+  email: 'admin@example.com',
+  password: 'password'
+)
+
+# Create posts and attached comments
+4.times do
+  post = Post.create(
+    title: Faker::Lorem.sentence,
+    subtitle: Faker::Lorem.sentence,
+    text: Faker::Lorem.paragraph
+  )
+  3.times do
+    post.comments.create(
+      commenter: Faker::Internet.email,
+      body: Faker::Lorem.sentence
+    )
+  end
+end
