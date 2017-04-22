f2e9a0465ed75dce7ab947fb116fa697bde3f4f9
Sean A. Abdoli <mauveisfine@gmail.com>
2017-04-21 21:58:15 -0500

    Remove commenter attribute from Comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170422004716) do
+ActiveRecord::Schema.define(version: 20170422024349) do
 
   create_table "comments", force: :cascade do |t|
-    t.string   "commenter"
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -2,7 +2,6 @@ require 'faker'
 
 FactoryGirl.define do
   factory :comment do |f|
-    f.commenter { Faker::Internet.email }
     f.body { Faker::Lorem.paragraph }
     f.post
     f.user
