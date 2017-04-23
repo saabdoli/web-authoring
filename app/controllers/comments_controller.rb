db871699ca44fc3142586ce24f5f511904f07ff9
Sean A. Abdoli <mauveisfine@gmail.com>
2017-04-22 19:00:57 -0500

    Fixed comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,22 +3,29 @@ class CommentsController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.create(comment_params)
+    @comment = @post.comments.build(comment_params)
     @comment.user = current_user
 
-    redirect_to post_path(@post)
+    if @comment.save!
+      redirect_to post_path(@post)
+    else
+      render 'Comment creation failed'
+    end
   end
 
   def destroy
     @post = Post.find(params[:post_id])
     @comment = @post.comments.find(params[:id])
-    @comment.destroy
-    redirect_to post_path(@post)
+    if @comment.destroy
+      redirect_to post_path(@post)
+    else
+      render 'Comment deletion failed'
+    end
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:commenter, :body, :user_id)
+    params.require(:comment).permit(:body)
   end
 end
